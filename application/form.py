77fcf114551dd52391d0399aad884f98bccf6730
Teesha2004 <teeshabakshi04@gmail.com>
2025-03-26 23:21:38 +0530

    inital commit

---
+++ application/form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField, IntegerField,PasswordField
+from wtforms.validators import DataRequired , Length
+
+class UserDataForm(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                                choices=[('income', 'income'),
+                                        ('expense', 'expense')])
+    category = SelectField("Category", validators=[DataRequired()],
+                                            choices =[('rent', 'rent'),
+                                            ('salary', 'salary'),
+                                            ('investment', 'investment'),
+                                            ('side_hustle', 'side_hustle')
+                                            ]
+                            )
+    amount = IntegerField('Amount', validators = [DataRequired()])                                   
+    submit = SubmitField('Generate Report')                            
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')                      
